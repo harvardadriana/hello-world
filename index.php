b74914af755613919a4d1a0dc3144d8d9223a800
harvardadriana <adrianarossettisugih@g.harvard.edu>
2017-02-07 23:11:11 +0900

    index.php deleted

--- index.php
+++
@@ -1,7 +0,0 @@
-<h1>Hello World</h1>
-<<<<<<< HEAD
-<img src="/images/spa.jpg" alt="my spa" />
-<a href="http://www.google.com/">Google</a>
-=======
-<img src="/images/spa.jpg alt="my spa" />
->>>>>>> b9584f08b29fa1afb31908b1d29ef9b0d523e9ca
